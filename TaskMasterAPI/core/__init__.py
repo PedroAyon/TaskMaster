8a859e0dde44ab6f2751debbe3135118dd7c9615
Pedro Ayon <pedroayonb@gmail.com>
2023-12-02 20:59:02 -0600

    CORS added, auth endpoints modified

--- TaskMasterAPI/core/__init__.py
+++ TaskMasterAPI/core/__init__.py
@@ -2,12 +2,14 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 
 load_dotenv()
 
 mysql_username = os.getenv('MYSQL_USER')
 mysql_password = os.getenv('MYSQL_PASSWORD')
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{mysql_username}:{mysql_password}@localhost:3306/task_master'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
