bf0ab762a1c1addeb0e900cc0945a8c4d7c4e6ec
Neto616 <basn160603@gmail.com>
2023-12-06 16:22:08 -0600

    repositorio de autenticacion terminado

--- task_master/lib/data/repository/auth_repository_impl.dart
+++ task_master/lib/data/repository/auth_repository_impl.dart
@@ -1,33 +1,92 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:task_master/domain/model/user.dart';
 import 'package:task_master/domain/repository/auth_repository.dart';
+import 'package:task_master/util/constants.dart';
+import 'package:http/http.dart' as http;
+import 'package:task_master/util/utils.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
-  final _storage = const FlutterSecureStorage();
+  final LocalStorage _storage = LocalStorage('auth');
+  final _logInURL = '$baseURL/login';
+  final _signUpURL = '$baseURL/signup';
+  final _userURL = '$baseURL/user';
 
   @override
-  Future<bool> logIn(String email, String password) {
-    // TODO: implement logIn
-    throw UnimplementedError();
+  Future<String?> logIn(String email, String password) async {
+    var data = <String, dynamic>{};
+    data['email'] = email;
+    data['password'] = password;
+    final response =
+        await http.post(Uri.parse(_logInURL), body: data);
+    var responseBody = json.decode(response.body);
+    if (!response.statusCode.isStatusOk()) return responseBody['message'];
+    await saveJWT(responseBody['token']);
+    return null;
   }
 
   @override
   Future<void> saveJWT(String jwt) async {
-    await _storage.write(key: 'jwt', value: jwt);
+    await _storage.ready;
+    _storage.setItem('jwt', jwt);
   }
 
   @override
-  Future<bool> signUp(String name, String email, String password) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<String?> signUp(String name, String email, String password) async {
+    var data = <String, dynamic>{};
+    data['email'] = email;
+    data['name'] = name;
+    data['password'] = password;
+    final response =
+        await http.post(Uri.parse(_signUpURL), body: data);
+    if (!response.statusCode.isStatusOk()) {
+      var responseBody = json.decode(response.body);
+      return responseBody['message'];
+    }
+    return null;
   }
 
   @override
   Future<String?> getJWT() async {
-    return await _storage.read(key: 'jwt');
+    await _storage.ready;
+    return  _storage.getItem('jwt');
   }
 
   @override
   Future<void> logOut() async {
-    await _storage.delete(key: 'jwt');
+    await _storage.ready;
+    _storage.deleteItem('jwt');
   }
+
+  @override
+  Future<bool> userLoggedIn() async {
+    return await getJWT() != null;
+  }
+
+  @override
+  Future<User?> getUser() async {
+    if (!await userLoggedIn()) return null;
+    final response = await http.get(Uri.parse(_userURL), headers: {
+      'taskmaster-access-token':
+      await getJWT() ?? ''
+    });
+    return User.fromJson(json.decode(response.body));
+  }
+}
+
+void main() async {
+  debugPrint("AUTH DEBUG");
+  var repo = AuthRepositoryImpl();
+  String? message = await repo.logIn('pedroayonb@gmail.com', 'pjab260803');
+  if (message == null) {
+    debugPrint(await repo.getJWT());
+  } else {
+    debugPrint(message);
+  }
+  User? user = await repo.getUser();
+  if (user == null) debugPrint('User is null!');
+  debugPrint('${user!.id}, ${user.name}');
+  await repo.logOut();
 }
